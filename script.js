fe3a5afc4d41bde068a509b49fd426eab9a2108d
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2024-10-22 18:52:05 -0400

    Complete functions that allow coloring & resizing grid

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
+//Element Queries
 var mainContainer = document.getElementById("main-container");
+var gridSize = 16;
+var body = document.getElementById("body"); 
+var button = document.getElementById("button");
 
-for (let u = 0; u < 256; u++) {
-    var box = document.createElement("div");
-    box.className = "box";
-    mainContainer.appendChild(box);
+//Initial Grid 16x16
+createGrid(mainContainer);
+
+//Event Listeners
+mainContainer.addEventListener("mouseover", changeColor); //Changes box color when mouseover
+button.addEventListener("click", sizePrompt); //Prompts user to change grid size when button clicked
+
+
+//FUNCTIONS
+function createGrid (container) {
+    for (let u = 0; u < gridSize * gridSize; u++) {
+        var box = document.createElement("div");
+        box.className = "box";
+        container.appendChild(box);
+    }
+    setBoxSize(gridSize);
 }
 
-mainContainer.addEventListener("mouseover", changeColor);
+function setBoxSize () {
+    var boxes = document.getElementsByClassName("box");
+    var sideLength = 992/gridSize;
+    var arr = Array.from(boxes);
+    arr.forEach((element) => {
+        element.style.height = sideLength + "px";
+        element.style.width = sideLength + "px";
+    })
+}
 
 function changeColor (e) {
-    e.target.style.backgroundColor = "#ff7f50";
+    if (e.target.classList.contains("box")){
+        e.target.style.backgroundColor = "#ff7f50";
+    }
+}
+
+function sizePrompt () {
+    gridSize = prompt("How many squares per side? Please enter a square number.", 16);
+    if (gridSize === null) gridSize = 16;
+    button.textContent = "Grid Size: " + gridSize;
+    if (gridSize <= 100 && Number.isInteger(Math.sqrt(gridSize))) {
+        const container = document.getElementById("main-container");
+        container.remove();
+        const newContainer = document.createElement("div");
+        newContainer.setAttribute("id", "main-container");
+        body.appendChild(newContainer)
+        createGrid(newContainer);
+        setBoxSize();
+        newContainer.addEventListener("mouseover", changeColor);
+    } else sizePrompt();
 }
